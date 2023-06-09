c68f77d55c6a4b387fe79a3b1a9c8314fda100ee
Dave86dev <davidochando1986@gmail.com>
2023-06-09 10:36:21 +0200

    localStorage además de conversion de guardado de objetos a session y localStorage

--- js/about.js
+++ js/about.js
@@ -2,4 +2,12 @@
 let mensaje = document.getElementById("mensaje");
 
 
-mensaje.innerHTML = `${sessionStorage.getItem("nombre")}`;
+// mensaje.innerHTML = `${sessionStorage.getItem("nombre")}`;
+// let item = localStorage.getItem("nombre")
+// mensaje.innerHTML = `${item}`;
+
+let objetoRecuperado = JSON.parse(sessionStorage.getItem("pelis"));
+
+console.log(objetoRecuperado)
+
+mensaje.innerHTML = `${objetoRecuperado.pelicula3}`;
--- js/main.js
+++ js/main.js
@@ -6,11 +6,33 @@ const saveData = () => {
 
     //Ahora guardo en el sessionStorage....
 
-    sessionStorage.setItem("nombre", inputName.value);
+    // sessionStorage.setItem("nombre", inputName.value);
+
+    // localStorage.setItem("nombre",inputName.value)
+
+    // //clear lo limpia todo...
+    // localStorage.clear();
+
+    // //removeItem quita algo en concreto
+    // localStorage.removeItem("nombre");
 
     //////////////////////////////////////////////////
 
     //Ahora redirigire a otra página
+
+
+
+
+
+    //guardando un objeto de JavaScript
+    let objetoMovies = {
+        pelicula1 : "Pulp Fiction",
+        pelicula2 : "El señor de los Anillos",
+        pelicula3 : "Piratas del Caribe"
+    }
+
+    //Tenemos que hacer un JSON stringify para que guarde el objeto películas como un string
+    sessionStorage.setItem("pelis", JSON.stringify(objetoMovies));
 
     window.location.href = "./pages/about.html";
 }
